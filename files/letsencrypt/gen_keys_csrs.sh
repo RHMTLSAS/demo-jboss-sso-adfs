630f956961480f905682a5591403a84cdb7d17be
Sebastien Perreault <sperreault@alesium.net>
2018-09-04 12:36:46 -0400

    last release for on-prem adfs

--- files/letsencrypt/gen_keys_csrs.sh
+++ files/letsencrypt/gen_keys_csrs.sh
@@ -24,8 +24,10 @@ function gen_dhparam
 # Generate the keys
 gen_key account.key
 gen_key adfs.key
+gen_key rhsso.key
 # Generate the dhparams
 gen_dhparam dhparams.out
 
 openssl req -new -sha256 -key adfs.key -subj "/" -reqexts SAN -config <(cat openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:adfs.sso.doogie.ca,DNS:enterpriseregistration.sso.doogie.ca")) > adfs.csr
+openssl req -new -sha256 -key rhsso.key -subj "/" -reqexts SAN -config <(cat openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:rhsso.sso.doogie.ca")) > rhsso.csr
 
